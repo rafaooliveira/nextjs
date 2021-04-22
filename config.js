1e421249b14b18470056a62966b709760afe1ac9
rafaooliveira <raafasantos1@gmail.com>
2021-04-22 15:48:51 -0300

    comentario novo

---
+++ config.js
@@ -0,0 +1,12 @@
+// // eslint-disable-next-line @typescript-eslint/no-var-requires
+// const withImages = require('next-images')
+// module.exports = {
+//   images: {
+//     domains: [
+//     ]
+//   },
+//   ...withImages({
+//     esModule: true,
+//     target: 'serverless'
+//   })
+// }
---
+++ pages/api/client.js
@@ -0,0 +1,4 @@
+
+import { GraphQLClient } from 'graphql-request'
+const endpoint = 'http://localhost:1337/graphql'
+export const client = new GraphQLClient(endpoint, { headers: {'Accept': 'application/json'} })
