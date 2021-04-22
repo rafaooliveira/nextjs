4eda55afe6ebae6bb4f42222c57a4776a0a2a62a
rafaooliveira <raafasantos1@gmail.com>
2021-04-22 17:34:48 -0300

    Mais alterações

--- pages/api/client.js
+++ pages/api/client.js
@@ -2,3 +2,4 @@
 import { GraphQLClient } from 'graphql-request'
 const endpoint = 'http://localhost:1337/graphql'
 export const client = new GraphQLClient(endpoint, { headers: {'Accept': 'application/json'} })
+// mudança
