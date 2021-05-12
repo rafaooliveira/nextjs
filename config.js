11aa869ff5ca3cff405eb2b57618d93879d0e357
rafaooliveira <raafasantos1@gmail.com>
2021-05-12 14:59:28 -0300

    config.js configurado

--- config.js
+++ config.js
@@ -10,3 +10,15 @@
 //     target: 'serverless'
 //   })
 // }
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  }
+}
