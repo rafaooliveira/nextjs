b686e89b58e35eedaed6de5bb3cb6daf9047cd98
rafaooliveira <raafasantos1@gmail.com>
2021-05-13 16:16:03 -0300

    mudanças na estrutura

--- pages/api/client.js
+++
@@ -1,4 +0,0 @@
-
-// import { GraphQLClient } from 'graphql-request'
-// const endpoint = 'http://localhost:1337/graphql'
-// export const client = new GraphQLClient(endpoint, { headers: {'Accept': 'application/json'} })
