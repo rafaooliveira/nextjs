8224772e2e1fae4b97133d7f2b42ae52140e920b
rafaooliveira <raafasantos1@gmail.com>
2021-05-13 15:55:44 -0300

    editar next.config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const isProd = process.env.NODE_ENV === 'production';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { parsed: localEnv } = require('dotenv').config();
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const webpack = require('webpack');
 
 module.exports = {
   webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+    
     config.node = { fs: 'empty' };
-    config.plugins = config.plugins || [];
 
-    config.plugins = [...config.plugins];
 
     return config;
   }
